3133d9c36a92927a947639f91165438df40cad30
ianrob <ianrob@amazon.co.uk>
2025-10-15 14:18:08 +0100

    Filter warnings

--- lexical-graph/src/graphrag_toolkit/lexical_graph/visualisation/__init__.py
+++ lexical-graph/src/graphrag_toolkit/lexical_graph/visualisation/__init__.py
@@ -1,4 +1,8 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+import warnings
 from .graph_notebook import *
+from warnings import *
+
+warnings.filterwarnings('ignore', category=FutureWarning)
