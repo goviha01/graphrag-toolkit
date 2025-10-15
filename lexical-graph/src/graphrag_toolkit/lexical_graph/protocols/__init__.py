afe809a25d10369fb810200b9c0c264df5a23dea
ianrob <ianrob@amazon.co.uk>
2025-10-15 14:25:20 +0100

    Filter warnings

--- lexical-graph/src/graphrag_toolkit/lexical_graph/protocols/__init__.py
+++ lexical-graph/src/graphrag_toolkit/lexical_graph/protocols/__init__.py
@@ -1,5 +1,9 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+import warnings
 from .mcp_server import create_mcp_server
 from .mcp_server import ToolParameters, UpdateParametersFunction
+from warnings import *
+
+warnings.filterwarnings('ignore', category=DeprecationWarning)
